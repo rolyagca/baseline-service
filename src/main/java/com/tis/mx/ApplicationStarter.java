d20f715dadd24ad846856bf8fcae4181210774ac
rolyagca <rolyagca@hotmail.com>
2021-09-14 13:37:50 -0500

    Dependencias cloud

--- src/main/java/com/tis/mx/ApplicationStarter.java
+++ src/main/java/com/tis/mx/ApplicationStarter.java
@@ -17,11 +17,13 @@ package com.tis.mx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * The Class ApplicationStarter.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationStarter {
   
   /**
