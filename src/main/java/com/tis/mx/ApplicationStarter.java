7ba4b2c4755d703cad90b61c05a1b76fe05a7ca7
rolyagca <rolyagca@hotmail.com>
2021-09-15 23:39:37 -0500

    Agregado cors compatibility

--- src/main/java/com/tis/mx/ApplicationStarter.java
+++ src/main/java/com/tis/mx/ApplicationStarter.java
@@ -18,12 +18,16 @@ package com.tis.mx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * The Class ApplicationStarter.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class ApplicationStarter {
   
   /**
